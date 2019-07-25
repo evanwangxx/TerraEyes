7ad0b3b93fa51807aabf41e06e1e265ff048fe91
chenlixin <chenlixin_sjtu@163.com>
2019-07-25 10:17:31 +0800

    add label,circle

--- docs/bin/map/object/circle.js
+++ docs/bin/map/object/circle.js
@@ -1,4 +1,60 @@
 // circle.js
 // map-circle library for TerraEyes
 // (c) 2019 Hongbo Wang
+// (c) 2019 Lynx Chen
 // Copyright © 1998 - 2019 Tencent. All Rights Reserved.
+
+const Circle = class {
+    constructor(map, option = "other") {
+        this.map = map;
+        this.height = 1000;
+        this.strokeWeight = 1;
+        this.fillWeight = 0.05;
+        this.option = option
+    }
+
+    setFillWeight(fillWeight){
+        this.fillWeight = parseInt(fillWeight)
+    }
+
+    setStrokeWeight(strokeWeight){
+        this.strokeWeight = parseInt(strokeWeight)
+    }
+
+
+    setCircle(center, radius, color = '#FA5858') {
+        if (this.option === 'dashcircle') {
+            this.option = {
+                map: this.map,
+                center: center,
+                radius: radius,
+                strokeWeight: this.strokeWeight,
+                strokeDashStyle: 'dash',
+                cursor: 'pointer',
+                visible: true,
+                fillColor: qq.maps.Color.fromHex(color, this.fillWeight),
+                zIndex: this.height
+            }
+        } else {
+            this.option = {
+                map: this.map,
+                center: center,
+                radius: radius,
+                strokeColor: color,
+                fillColor: null,
+                strokeDashStyle: 'dash',
+                strokeWeight: this.strokeWeight
+            }
+        };
+        new qq.maps.Circle(this.option)
+    }
+
+    setCircleLayer(data) {
+        for (var i = 0; i < data.length; i++) {
+            let row = data[i];
+            let center = new qq.maps.LatLng(row.lat, row.lng);
+            let radius = row.radius;
+            this.setCircle(center, radius);
+        }
+    }
+};
--- docs/bin/map/object/label.js
+++ docs/bin/map/object/label.js
@@ -1,4 +1,42 @@
-// label.js
-// map-label library for TerraEyes
-// (c) 2019 Hongbo Wang
+// bubble.js
+// map-bubble library for TerraEyes
+// (c) 2019 Lynxchen
 // Copyright © 1998 - 2019 Tencent. All Rights Reserved.
+
+const Label = class {
+    constructor(map, elementId="visible-label") {
+        this.map = map;
+        this.height = 1000;
+        this.style = {color: "#242424", fontWeight: "bold"};
+        this.elementId = elementId
+    }
+
+    setLabel(center, text) {
+        let label = new qq.maps.Label({
+            map: this.map,
+            position: center,
+            zIndex: this.height,
+            content: text,
+            style: this.style
+        });
+
+        let visible = document.getElementById(this.elementId);
+        qq.maps.event.addDomListener(visible, "click", function () {
+            label.setMap(map);
+            if (label.getVisible()) {
+                label.setVisible(false)
+            } else {
+                label.setVisible(true)
+            }
+        });
+    }
+
+    setLabelLayer(data) {
+        for (var i = 0; i < data.length; i++) {
+            let row = data[i];
+            let center = new qq.maps.LatLng(row.lat, row.lng);
+            let text = row.text;
+            this.setLabel(center, text);
+        }
+    }
+};
