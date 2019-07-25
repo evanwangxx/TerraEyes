1c29f1beeee5bcd35e3bc9d5c8f4aecf09d0e256
chenlixin <chenlixin_sjtu@163.com>
2019-07-25 11:53:48 +0800

    bubble

--- docs/bin/map/object/bubble.js
+++ docs/bin/map/object/bubble.js
@@ -1,4 +1,57 @@
 // bubble.js
 // map-bubble library for TerraEyes
 // (c) 2019 Hongbo Wang
+// (c) 2019 Lynxchen
 // Copyright © 1998 - 2019 Tencent. All Rights Reserved.
+
+const Bubble = class {
+    constructor(map, level) {
+        this.map = map;
+        this.height = 1000;
+        this.strokeWeight = 0;
+        this.strokColor = "#FA5858";
+        this.level = level;
+        this.alpha = 0.5
+    }
+
+    setAlpha(alpha){
+        this.alpha = parseFloat(alpha)
+    }
+
+    setStrokeWeight(strokeWeight){
+        this.strokeWeight = parseFloat(strokeWeight)
+    }
+
+    setStrokColor(strokColor){
+        this.strokColor = strokColor
+    }
+
+    setBubble(point, radius) {
+        let level = this.level;
+        let option = {
+            map: this.map,
+            center: point,
+            radius: radius,
+            strokColor: qq.maps.Color.fromHex(this.strokColor, this.alpha),
+            strokWeight: this.strokeWeight,
+            visible: true,
+            fillColor: qq.maps.Color.fromHex(this.strokColor, this.alpha)
+        };
+
+        qq.maps.event.addListener(new qq.maps.Circle(option), 'click', function () {
+            let info = new qq.maps.InfoWindow({map: this.map});
+            info.open();
+            info.setContent('<div style="text-align:center;white-space:nowrap;' + 'margin:10px;">' + "量级：" + level + "<br>半径：" + radius.toFixed(2) + '</div>');
+            info.setPosition(point);
+        });
+    }
+
+    setBubbleLayer(data){
+        for (var i = 0; i < data.length; i++) {
+            let row = data[i];
+            let point = new qq.maps.LatLng(row.lat, row.lng);
+            let radius = row.radius;
+            this.setBubble(point, radius);
+        }
+    }
+};   
