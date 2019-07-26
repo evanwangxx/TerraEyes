2afb2c3fd9bfe0e04e0de6f0702e78ed2a8f3614
bird926 <45197247+bird926@users.noreply.github.com>
2019-07-26 17:02:22 +0800

    bug fixed
    bug fixed

--- docs/bin/map/object/polygon.js
+++ docs/bin/map/object/polygon.js
@@ -25,16 +25,16 @@ const Polygon = class {
     };
 
     /**
-     * 绘制多边形
+     * 绘制一个多边形
      *
-     * @param {Array} polygonArray 多边形经纬度点组成的数组
+     * @param {Array} pointsArray 多边形经纬度点组成的数组
      * @param {string} color 边框和区域的填充颜色
      * @param {number} fillOpacity 区域填充的不透明度，取值[0, 1]，1表示完全不透明
      */
-    addPolygon(polygonArray, color, fillOpacity) {
+    addPolygon(pointsArray, color, fillOpacity) {
         let polygon = new qq.maps.Polygon({
             map: this.map,
-            path: polygonArray,
+            path: pointsArray,
             strokeColor: color,
             strokeWeight: this.strokeWeight,
             fillColor: qq.maps.Color.fromHex(color, fillOpacity),
@@ -53,6 +53,22 @@ const Polygon = class {
             });
         } catch (err) {
             console.log(err.message);
+        }
+    };
+
+    /**
+     * 绘制一组多边形
+     * @param {Array} polygonArray 多边形数组，里面含多个json格式数据, 例如：
+     *   [{"pointsArray": pointsArr1, "color": "#FEB90F", "fillOpacity": 0.5},
+     *    {"pointsArray": pointsArr2, "color": "#FEB90F", "fillOpacity": 0.1}]
+     * */
+    addLayerOfPolygon(polygonArray) {
+        for (let i = 0; i < polygonArray.length; i++) {
+            console.log(polygonArray[i])
+            let pointsArray = polygonArray[i]["pointsArray"];
+            let color = polygonArray[i]["color"];
+            let fillOpacity = parseFloat(polygonArray[i]["fillOpacity"]);
+            this.addPolygon(pointsArray, color, fillOpacity);
         }
     }
 };
